d11eed94cb5fb45b29955dbbbfaec70a23a532e9
Chanry <chanry@SHUAIdeiMac.fios-router.home>
2020-08-26 15:14:34 -0400

    new tier1 uplaod

--- Revassess/src/main/java/com/revature/tier1/SumOverArray.java
+++ Revassess/src/main/java/com/revature/tier1/SumOverArray.java
@@ -5,8 +5,8 @@ public class SumOverArray {
 	public static int IterateAndSum(int[] arr) {
 		
 		int sum = 0; 
-		for(int  i=0; i<arr.length; i++) {
-			sum += arr[i];
+		for(int i =0; i<arr.length; i++ ) {
+			sum = sum+ arr[i];
 		}
 		return sum;
 	}
