01f98ffac5060e3792865281b26cdba574410203
Chanry <chanry@SHUAIdeiMac.fios-router.home>
2020-08-26 15:00:58 -0400

    finished tier 1

--- Revassess/src/main/java/com/revature/tier1/CompareStrings.java
+++ Revassess/src/main/java/com/revature/tier1/CompareStrings.java
@@ -2,7 +2,12 @@ package com.revature.tier1;
 
 public class CompareStrings {
 
-    public static boolean compareStrings(String s1, String s2){
-        return false;
-    }
+	public static boolean compareStrings(String s1, String s2) {
+
+		if (s1.equals(s2)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
--- Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
+++ Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
@@ -1,8 +1,28 @@
 package com.revature.tier1;
 
+import java.util.ArrayList;
+
 public class NumberSumLength {
 
 	public static boolean checkNumberPowerLength(long num) {
-		return false;
+
+		int sum = 0;
+		ArrayList<Integer> array = new ArrayList<Integer>();
+		do {
+			array.add((int) (num % 10));
+			num /= 10;
+
+		} while (num > 0);
+		int power = array.size();
+		for (int i : array) {
+			sum = sum + (int)Math.pow(i, power);
+		}
+		if (sum == num) {
+			return true;
+		} else {
+
+			return false;
+		}
+
 	}
 }
