44ae777d621c080065df7b5ca54042cd0bd29d0d
Chanry <chanry@SHUAIdeiMac.fios-router.home>
2020-08-26 16:12:24 -0400

    tier 2 first submit

---
+++ Revassess/src/sql/answer2.sql
@@ -0,0 +1,2 @@
+INSERT INTO APP_USER (user_id, username, PASSWORD, first_name, last_name, role_id)
+VALUES (28, 'RevUser', 12345, 'Chanry', 'Shuai', 4);
---
+++ Revassess/src/sql/answer3.sql
@@ -0,0 +1,2 @@
+SELECT * FROM study_set 
+JOIN app_user ON (app_user.user_id=5) ;
