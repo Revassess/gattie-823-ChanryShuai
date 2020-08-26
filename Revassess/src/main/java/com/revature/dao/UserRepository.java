3517c70b2ef6669e39026268151d910003882a3c
Chanry <chanry@SHUAIdeiMac.fios-router.home>
2020-08-26 17:51:53 -0400

    tier3456 update1

--- Revassess/src/main/java/com/revature/dao/UserRepository.java
+++ Revassess/src/main/java/com/revature/dao/UserRepository.java
@@ -5,6 +5,36 @@ import java.util.Set;
 import com.revature.model.User;
 
 public class UserRepository implements CrudRepository<User>{
+
+	@Override
+	public User save(User t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<User> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User findById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean update(User t) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteById(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 	
 	
 
